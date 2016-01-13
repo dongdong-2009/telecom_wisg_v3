f635144df946f303aeafcf7c81b0ae0c41c34ce0
张亚东 <1357244730@qq.com>
2016-01-13 09:23:05 +0800

    delet unused files

--- msgdef/include/msgtypedef_rtc.h
+++
@@ -1,8 +0,0 @@
-#ifndef MSGTYPEDEF_RTC_H_
-#define MSGTYPEDEF_RTC_H_
-
-#include "comtypedef_uninet.h"
-
-const TUniNetMsgType RTC_TYPE = 1060;
-
-#endif
--- msgdef/include/rtc_msg_def.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RTC_MSG_DEF_H
-#define _RTC_MSG_DEF_H
-
-#include "msgdef_rtc.h"
-#include "msgtypedef_rtc.h"
-#include "msgnamedef_rtc.h"
-
-#endif
